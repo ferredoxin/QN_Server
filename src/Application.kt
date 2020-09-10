1f20d5bd5b881ca0d26dccd1280b700b7a679396
singleNeuron <Cryolitia@gmail.com>
2020-09-10 16:54:04 +0200

    First Commit

---
+++ src/Application.kt
@@ -0,0 +1,45 @@
+package me.singleNeuron
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+        post("/webhook/github") {
+            call.application.environment.log.debug(call.receiveText())
+            call.respond("success")
+        }
+        post("/webhook/appcenter") {
+            call.application.environment.log.debug(call.receiveText())
+            call.respond("success")
+        }
+    }
+}
+
